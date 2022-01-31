729c99277fa9a9627563ecc3ba7d5c8053749fc7
serhatengin <serhat.developer@gmail.com>
2022-01-31 23:13:34 +0300

    connection string.

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,1 +1,21 @@
 package config
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	dsn := "host=localhost user=serhatengin password=gks123 dbname=edu_bookstore_db port=5432 sslmode=disable"
+	d, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	db = d
+}
