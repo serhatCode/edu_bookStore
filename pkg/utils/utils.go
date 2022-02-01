411418e8351c841554c95c6d6ada16290582dc87
serhatengin <serhat.developer@gmail.com>
2022-02-02 00:25:57 +0300

    fixed utils.

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func ParseBody(r *http.Request, X interface{})  {
+func ParseBody(r *http.Request, x interface{}) {
 	if body, err := ioutil.ReadAll(r.Body); err == nil {
 		if err := json.Unmarshal([]byte(body), x); err != nil {
 			return
